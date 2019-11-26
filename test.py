a3d66642da93bad94bd9e5480cef937c6d7b21ed
Alpha Hinex <AlphaHinex@gmail.com>
2019-11-26 20:09:21 +0800

    Add test case

--- test.py
+++ test.py
@@ -7,7 +7,9 @@ class TestStringMethods(unittest.TestCase):
     def test_images(self):
         # self.assertEqual(decoder('test.jpg'), '1bda2')
         # self.assertEqual(decoder('test2.jpg'), '7f4ca')
-        self.assertEqual(decoder('imageRandeCode.jpg'), '1234')
+
+        self.assertEqual(decoder('imageRandeCode.jpg'), '6263')
+        self.assertEqual(decoder('raw-material/0.jpg'), '0619')
 
 if __name__ == '__main__':
     unittest.main()
